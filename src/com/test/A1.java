4653ce33c954b85f40ae89a018773e322cb45372
dheerajgupta8108 <dheerajgupta8108@gmail.com>
2017-11-04 23:36:13 +0530

    Update A1.java

--- src/com/test/A1.java
+++ src/com/test/A1.java
@@ -6,7 +6,7 @@ public class A1 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+		System.out.println("max: "+currentMAx);
 
 		int n = sc.nextInt();
 		int[] arr = new int[n];
