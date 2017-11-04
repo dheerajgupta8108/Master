3b4b781c571350ba1b5a8d50948755455d150209
Dheeraj Gupta <dheerajgupta8108@gmail.com>
2017-11-04 23:34:54 +0530

    update

--- src/com/test/A1.java
+++ src/com/test/A1.java
@@ -6,7 +6,7 @@ public class A1 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+//		Scanner sc = new Scanner(System.in);
 
 		int n = sc.nextInt();
 		int[] arr = new int[n];
