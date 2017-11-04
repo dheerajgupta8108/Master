57e22ad0ab3249e1e80848c7959db79b17e225c0
dheerajgupta8108 <dheerajgupta8108@gmail.com>
2017-11-04 23:50:19 +0530

    Update A1.java

--- src/com/test/A1.java
+++ src/com/test/A1.java
@@ -9,6 +9,7 @@ public class A1 {
 		System.out.println("max: "+currentMAx);
 
 		int n = sc.nextInt();
+System.out.println("max: "+currentMAx);		
 		int[] arr = new int[n];
 		for(int i = 0; i<n; i++){
 			arr[i] = sc.nextInt();
